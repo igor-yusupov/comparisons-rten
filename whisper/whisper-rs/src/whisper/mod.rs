57c2d80d641148897bb350cff9e3b9f278d8026f
Igor Yusupov <yusupov.ir@protonmail.com>
2024-07-07 23:03:36 +0500

    Remove unnecessary trait

--- whisper/whisper-rs/src/whisper/mod.rs
+++ whisper/whisper-rs/src/whisper/mod.rs
@@ -60,195 +60,34 @@ impl fmt::Debug for Whisper {
     }
 }
 
-trait Recognition {
-    fn get_encoder(&self) -> &Model;
+impl Whisper {
+    pub fn new(
+        encoder_path: &str,
+        decoder_path: &str,
+        tokenizer_path: &str,
+        mel_filters_path: &str,
+        version: WhisperVersion,
+    ) -> Whisper {
+        let encoder = Model::load_file(encoder_path).unwrap();
+        let decoder = Model::load_file(decoder_path).unwrap();
+        let tokenizer = Tokenizer::new(tokenizer_path);
+        let mel_filters = get_mel_filteres(mel_filters_path);
+        let options = Options::new();
 
-    fn get_decoder(&self) -> &Model;
-
-    fn get_tokenizer(&self) -> &Tokenizer;
-
-    fn get_mel_filters(&self) -> &Array2<f32>;
-
-    fn get_options(&self) -> &Options;
-
-    fn get_default_kvcache(&self) -> KVCache;
-
-    fn inference_logits(
-        &self,
-        tokens: ArrayView2<i32>,
-        decoder_inputs: &[(NodeId, Output)],
-        kv_cache: &KVCache,
-        initial_token_length: usize,
-    ) -> (Array3<f32>, KVCache);
-
-    fn get_audio_features(&self, segments: Vec<Array2<f32>>) -> Array3<f32> {
-        let mels: Array3<f32> = segments
-            .into_iter()
-            .fold(None, |acc, array| {
-                Some(match acc {
-                    Some(concatenated) => {
-                        concatenate![Axis(0), concatenated, array.insert_axis(Axis(0))]
-                    }
-                    None => array.insert_axis(Axis(0)),
-                })
-            })
-            .unwrap();
-        let inputs = as_ndtensor_view(mels.view()).unwrap();
-        let encoder_out = self.get_encoder().run_one(inputs.into(), None).unwrap();
-        let result: NdTensor<f32, 3> = encoder_out.try_into().unwrap();
-        into_array(result)
-    }
-
-    fn get_initial_tokens(&self, prompt: Vec<i32>, language: &str) -> Vec<i32> {
-        let lang_token = *self.get_tokenizer().lang2token.get(language).unwrap();
-        let init_tokens: Vec<i32> = vec![50258, lang_token as i32, 50359];
-
-        if prompt.len() > 0 {
-            let prev_prompt_len = self.get_options().n_ctx / 2 - 1;
-            let prompt_tokens: Vec<i32>;
-
-            if prompt.len() > prev_prompt_len {
-                prompt_tokens = prompt[prompt.len() - prev_prompt_len..].to_vec();
-            } else {
-                prompt_tokens = prompt;
-            }
-
-            let tokens: Vec<i32> = vec![self.get_options().sot_prev as i32]
-                .into_iter()
-                .chain(prompt_tokens.into_iter())
-                .collect();
-            let tokens: Vec<i32> = tokens.into_iter().chain(init_tokens.into_iter()).collect();
-            tokens
-        } else {
-            let tokens = vec![self.get_options().sot_prev as i32];
-            let tokens: Vec<i32> = tokens.into_iter().chain(init_tokens.into_iter()).collect();
-            tokens
+        Whisper {
+            encoder,
+            decoder,
+            tokenizer,
+            mel_filters,
+            options,
+            version,
         }
     }
 
-    fn inference(
-        &self,
-        audio_features: ArrayView3<f32>,
-        prompt: Vec<i32>,
-        language: &str,
-    ) -> Vec<i32> {
-        let initial_tokens = self.get_initial_tokens(prompt, language);
-        let initial_token_length = initial_tokens.len();
-
-        let mut tokens: Array2<i32> = Array::from_vec(initial_tokens).insert_axis(Axis(0));
-        let mut kv_cache = self.get_default_kvcache();
-
-        // Precompute parts of the decoder graph that only depend on encoder
-        // outputs. This makes each decoder step much faster.
-        let decoder = self.get_decoder();
-        let logits_id = decoder.node_id("logits").unwrap();
-        let audio_features_id = decoder.node_id("audio_features").unwrap();
-        let audio_features_tensor = as_ndtensor_view(audio_features.view()).unwrap();
-        let decoder_inputs = decoder
-            .partial_run(
-                vec![(audio_features_id, audio_features_tensor.into())],
-                &[logits_id],
-                None,
-            )
-            .expect("decoder run failed");
-
-        for _ in 0..224 {
-            let logits: Array3<f32>;
-            (logits, kv_cache) = self.inference_logits(
-                tokens.view(),
-                &decoder_inputs,
-                &kv_cache,
-                initial_token_length,
-            );
-            let next_word = logits
-                .slice(s![.., -1, ..])
-                .iter()
-                .enumerate()
-                .max_by(|(_, u), (_, v)| u.total_cmp(v))
-                .map(|(i, _)| i as usize)
-                .unwrap();
-
-            if next_word == self.get_options().eot_token
-                || tokens.shape()[1] > self.get_options().n_ctx
-            {
-                break;
-            }
-
-            let next_word_array = Array::from_elem((1, 1), next_word as i32);
-            tokens = concatenate!(Axis(1), tokens, next_word_array);
-        }
-        tokens = tokens.slice(s![.., initial_token_length..]).to_owned();
-        tokens.into_raw_vec()
-    }
-
-    fn encode(&self, mel: Array2<f32>) -> Array3<f32> {
-        let num_frames = mel.shape()[1];
-        let mut seek = 0;
-        let mut segments = vec![];
-
-        while seek < num_frames {
-            let segment: Array2<f32>;
-
-            if seek + audio::N_FRAMES < mel.shape()[1] {
-                segment = mel.slice(s![.., seek..seek + audio::N_FRAMES]).to_owned();
-            } else {
-                segment = mel.slice(s![.., seek..]).to_owned();
-            }
-
-            segments.push(audio::pad_or_trim(segment, audio::N_FRAMES));
-            seek += audio::N_FRAMES;
-        }
-
-        self.get_audio_features(segments)
-    }
-
-    fn decode_tokens(&self, result: Vec<i32>) -> String {
-        self.get_tokenizer().decode(
-            result
-                .iter()
-                .map(|v| *v as usize)
-                .filter(|item| item < &50257)
-                .collect(),
-        )
-    }
-
-    fn get_mel(&self, audio_data: Vec<f32>) -> Array2<f32> {
-        audio::log_mel_spectrogram(audio_data, self.get_mel_filters().clone())
-    }
-
-    fn run(&self, mel: Array2<f32>, language: &str) -> String {
-        let audio_features = self.encode(mel);
-
-        let mut result: Vec<i32> = vec![];
-        for audio_feature in audio_features.axis_iter(Axis(0)) {
-            let audio_feature = audio_feature.insert_axis(Axis(0));
-            let tokens = self.inference(audio_feature, result.clone(), language);
-            result.extend(tokens.clone());
-        }
-
-        self.decode_tokens(result)
-    }
-}
-
-impl Recognition for Whisper {
-    fn get_encoder(&self) -> &Model {
-        &self.encoder
-    }
-
-    fn get_decoder(&self) -> &Model {
-        &self.decoder
-    }
-
-    fn get_tokenizer(&self) -> &Tokenizer {
-        &self.tokenizer
-    }
-
-    fn get_mel_filters(&self) -> &Array2<f32> {
-        &self.mel_filters
-    }
-
-    fn get_options(&self) -> &Options {
-        &self.options
+    pub fn recognize_from_audio(&self, audio_path: &str, language: &str) -> String {
+        let audio_data = read_audio(audio_path).unwrap();
+        let mel = self.get_mel(audio_data);
+        self.run(mel, language)
     }
 
     fn get_default_kvcache(&self) -> KVCache {
@@ -338,35 +177,150 @@ impl Recognition for Whisper {
 
         (logits, new_kv_cache)
     }
-}
 
-impl Whisper {
-    pub fn new(
-        encoder_path: &str,
-        decoder_path: &str,
-        tokenizer_path: &str,
-        mel_filters_path: &str,
-        version: WhisperVersion,
-    ) -> Whisper {
-        let encoder = Model::load_file(encoder_path).unwrap();
-        let decoder = Model::load_file(decoder_path).unwrap();
-        let tokenizer = Tokenizer::new(tokenizer_path);
-        let mel_filters = get_mel_filteres(mel_filters_path);
-        let options = Options::new();
+    fn get_audio_features(&self, segments: Vec<Array2<f32>>) -> Array3<f32> {
+        let mels: Array3<f32> = segments
+            .into_iter()
+            .fold(None, |acc, array| {
+                Some(match acc {
+                    Some(concatenated) => {
+                        concatenate![Axis(0), concatenated, array.insert_axis(Axis(0))]
+                    }
+                    None => array.insert_axis(Axis(0)),
+                })
+            })
+            .unwrap();
+        let inputs = as_ndtensor_view(mels.view()).unwrap();
+        let encoder_out = self.encoder.run_one(inputs.into(), None).unwrap();
+        let result: NdTensor<f32, 3> = encoder_out.try_into().unwrap();
+        into_array(result)
+    }
 
-        Whisper {
-            encoder,
-            decoder,
-            tokenizer,
-            mel_filters,
-            options,
-            version,
+    fn get_initial_tokens(&self, prompt: Vec<i32>, language: &str) -> Vec<i32> {
+        let lang_token = *self.tokenizer.lang2token.get(language).unwrap();
+        let init_tokens: Vec<i32> = vec![50258, lang_token as i32, 50359];
+
+        if prompt.len() > 0 {
+            let prev_prompt_len = self.options.n_ctx / 2 - 1;
+            let prompt_tokens: Vec<i32>;
+
+            if prompt.len() > prev_prompt_len {
+                prompt_tokens = prompt[prompt.len() - prev_prompt_len..].to_vec();
+            } else {
+                prompt_tokens = prompt;
+            }
+
+            let tokens: Vec<i32> = vec![self.options.sot_prev as i32]
+                .into_iter()
+                .chain(prompt_tokens.into_iter())
+                .collect();
+            let tokens: Vec<i32> = tokens.into_iter().chain(init_tokens.into_iter()).collect();
+            tokens
+        } else {
+            let tokens = vec![self.options.sot_prev as i32];
+            let tokens: Vec<i32> = tokens.into_iter().chain(init_tokens.into_iter()).collect();
+            tokens
         }
     }
 
-    pub fn recognize_from_audio(&self, audio_path: &str, language: &str) -> String {
-        let audio_data = read_audio(audio_path).unwrap();
-        let mel = self.get_mel(audio_data);
-        self.run(mel, language)
+    fn inference(
+        &self,
+        audio_features: ArrayView3<f32>,
+        prompt: Vec<i32>,
+        language: &str,
+    ) -> Vec<i32> {
+        let initial_tokens = self.get_initial_tokens(prompt, language);
+        let initial_token_length = initial_tokens.len();
+
+        let mut tokens: Array2<i32> = Array::from_vec(initial_tokens).insert_axis(Axis(0));
+        let mut kv_cache = self.get_default_kvcache();
+
+        // Precompute parts of the decoder graph that only depend on encoder
+        // outputs. This makes each decoder step much faster.
+        let logits_id = self.decoder.node_id("logits").unwrap();
+        let audio_features_id = self.decoder.node_id("audio_features").unwrap();
+        let audio_features_tensor = as_ndtensor_view(audio_features.view()).unwrap();
+        let decoder_inputs = self
+            .decoder
+            .partial_run(
+                vec![(audio_features_id, audio_features_tensor.into())],
+                &[logits_id],
+                None,
+            )
+            .expect("decoder run failed");
+
+        for _ in 0..224 {
+            let logits: Array3<f32>;
+            (logits, kv_cache) = self.inference_logits(
+                tokens.view(),
+                &decoder_inputs,
+                &kv_cache,
+                initial_token_length,
+            );
+            let next_word = logits
+                .slice(s![.., -1, ..])
+                .iter()
+                .enumerate()
+                .max_by(|(_, u), (_, v)| u.total_cmp(v))
+                .map(|(i, _)| i as usize)
+                .unwrap();
+
+            if next_word == self.options.eot_token || tokens.shape()[1] > self.options.n_ctx {
+                break;
+            }
+
+            let next_word_array = Array::from_elem((1, 1), next_word as i32);
+            tokens = concatenate!(Axis(1), tokens, next_word_array);
+        }
+        tokens = tokens.slice(s![.., initial_token_length..]).to_owned();
+        tokens.into_raw_vec()
+    }
+
+    fn encode(&self, mel: Array2<f32>) -> Array3<f32> {
+        let num_frames = mel.shape()[1];
+        let mut seek = 0;
+        let mut segments = vec![];
+
+        while seek < num_frames {
+            let segment: Array2<f32>;
+
+            if seek + audio::N_FRAMES < mel.shape()[1] {
+                segment = mel.slice(s![.., seek..seek + audio::N_FRAMES]).to_owned();
+            } else {
+                segment = mel.slice(s![.., seek..]).to_owned();
+            }
+
+            segments.push(audio::pad_or_trim(segment, audio::N_FRAMES));
+            seek += audio::N_FRAMES;
+        }
+
+        self.get_audio_features(segments)
+    }
+
+    fn decode_tokens(&self, result: Vec<i32>) -> String {
+        self.tokenizer.decode(
+            result
+                .iter()
+                .map(|v| *v as usize)
+                .filter(|item| item < &50257)
+                .collect(),
+        )
+    }
+
+    fn get_mel(&self, audio_data: Vec<f32>) -> Array2<f32> {
+        audio::log_mel_spectrogram(audio_data, self.mel_filters.clone())
+    }
+
+    fn run(&self, mel: Array2<f32>, language: &str) -> String {
+        let audio_features = self.encode(mel);
+
+        let mut result: Vec<i32> = vec![];
+        for audio_feature in audio_features.axis_iter(Axis(0)) {
+            let audio_feature = audio_feature.insert_axis(Axis(0));
+            let tokens = self.inference(audio_feature, result.clone(), language);
+            result.extend(tokens.clone());
+        }
+
+        self.decode_tokens(result)
     }
 }
